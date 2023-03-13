9dd30fcc7e4a6d2de38b98560ceb2ec4627a9d20
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2023-03-13 21:44:23 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [cosmetics, setCosmetics] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://fortnite-api.com/v2/cosmetics/br/search')
+      .then(response => {
+        setCosmetics(response.data.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Cosmetics List</h1>
+      <ul>
+        {cosmetics.map(cosmetic => (
+          <li key={cosmetic.id}>
+            <img src={cosmetic.images.icon} alt={cosmetic.name} />
+            {cosmetic.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
+
+export default App;
