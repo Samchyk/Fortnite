9cbbc394f4a07ce2e947c586deb5c8b0a19fdd7c
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2023-03-17 10:05:22 +0200

    17,03 (роут баг)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<List />} />
+      <Route path="/" element={<List/>} />
       <Route path="/:id" element={<SelectedCosmetic />} />
     </Routes>
   );
